5db679faab90da0c643d39d44a5b9037882d16cd
immanoj16 <mmaharana14@gmail.com>
2017-08-25 03:26:37 +0530

    Completed authenticate with google

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ passport.use(
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-  }, (accessToken) => {
-    console.log(accessToken);
+  }, (accessToken, refreshToken, profile, done) => {
+    console.log('accessToken', accessToken);
+    console.log('refresh token',refreshToken);
+    console.log('profile', profile);
   })
 );
 
@@ -22,6 +24,8 @@ app.get(
     scope: ['profile', 'email']
   })
 );
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
