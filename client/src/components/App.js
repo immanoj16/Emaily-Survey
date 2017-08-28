423e1e70c2612bbc6ed8b1aadddfb755dc628bef
immanoj16 <mmaharana14@gmail.com>
2017-08-28 17:25:32 +0530

    Add: materialize css

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,8 @@
-// eslint-disable-next-line
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-const Header = () => <h2>Header</h2>
+import Header from './Header'
+
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
