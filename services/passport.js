06bd3292df5b3b8098057f851dfa371234d7afac
immanoj16 <mmaharana14@gmail.com>
2017-08-28 15:20:22 +0530

    Add: react and in passport js use async to make the callback asynchronous

--- services/passport.js
+++ services/passport.js
@@ -19,23 +19,21 @@ passport.deserializeUser((id, done) => {
 
 passport.use(
   new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback',
-    proxy: true
-  },
-  (accessToken, refreshToken, profile, done) => {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+      proxy: true
+    },
+    async (accessToken, refreshToken, profile, done) => {
 
-    User.findOne({ googleId: profile.id })
-      .then((existingUser) => {
-        if (existingUser) {
-          // we already have a record with the given profile id
-          done(null, existingUser);
-        } else {
-          // we don't have a user record with this id, make a new one
-          new User({ googleId: profile.id }).save()
-            .then(user => done(null, user));
-        }
-      });
-  })
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
+    }
+  )
 );
