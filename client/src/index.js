1a197d47d8ba783c09ad64b54fa63f78d30a5db6
immanoj16 <mmaharana14@gmail.com>
2017-08-28 16:58:02 +0530

    Add: react route to App.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,4 @@
-// eslint-disable-next-line
-import React, { Component } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
