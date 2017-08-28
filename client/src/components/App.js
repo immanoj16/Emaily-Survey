3e02ed8a173c5bad1bb6b119a63769129d3319f9
immanoj16 <mmaharana14@gmail.com>
2017-08-28 15:59:41 +0530

    Added some redux stuff to index

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line
+import React, { Component } from 'react'
+
+const App = () => {
+  return (
+    <div>
+      Hi There!
+    </div>
+  );
+};
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// eslint-disable-next-line
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App'
+
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
