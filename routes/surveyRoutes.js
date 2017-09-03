79d8f1e2c4362599f4128888989ac7e5ed71ba83
immanoj16 <mmaharana14@gmail.com>
2017-09-03 21:05:40 +0530

    Add: mail template

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
@@ -21,6 +22,6 @@ module.exports = app => {
     });
 
     // Great place to send an email!
-    const mailer = new Mailer(survey, template);
+    const mailer = new Mailer(survey, surveyTemplate(survey));
   });
 };
