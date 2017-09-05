e6c46253766d085b761031f41f517dd5cebc98c7
immanoj16 <mmaharana14@gmail.com>
2017-09-05 14:37:19 +0530

    Add: email template

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -8,7 +8,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
 
     const { title, subject, body, recipients } = req.body;
 
@@ -23,5 +23,6 @@ module.exports = app => {
 
     // Great place to send an email!
     const mailer = new Mailer(survey, surveyTemplate(survey));
+    mailer.send();
   });
 };
