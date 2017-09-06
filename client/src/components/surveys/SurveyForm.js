de9205615d8e2e4f64434636080c3f7c5716f76d
immanoj16 <mmaharana14@gmail.com>
2017-09-07 00:24:34 +0530

    Add: validate to show validation errors

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,6 +1,7 @@
 import _ from 'lodash'
 import React, { Component } from 'react'
 import { reduxForm, Field } from 'redux-form'
+import { Link } from 'react-router-dom'
 
 import SurveyField from './SurveyField'
 
@@ -27,18 +28,38 @@ class SurveyForm extends Component {
     })
   }
 
-  render() {
+  render () {
     return (
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {this.renderFields()}
-          <button type="submit">Submit</button>
+          <Link to='/surveys' className="red btn-flat white-text">
+            Cancel
+            <i className="material-icons right">cancel</i>
+          </Link>
+          <button className="teal btn-flat right white-text" type="submit">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
         </form>
       </div>
     )
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  _.each(FIELDS, ({ name }) => {
+    if (!values[name]) {
+      errors[name] = `Required ${name}`
+    }
+  });
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'surveyForm'
 })(SurveyForm)
